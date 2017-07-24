6295685e10ea5a085a39c23af95ad9403cf095ff
one.0 <wyparks2@gmail.com>
2017-07-24 18:06:38 +0900

    Update - 소스 정리

--- src/main/java/io/github/wonyoungpark/ams/web/ProductController.java
+++ src/main/java/io/github/wonyoungpark/ams/web/ProductController.java
@@ -5,12 +5,9 @@ import io.github.wonyoungpark.ams.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * Created by one0 on 2017. 7. 1..
