4402c9843d6a76f5ae980a99f98da2fa74a941f5
one.0 <wyparks2@gmail.com>
2017-07-24 12:46:06 +0900

    Add - 시큐리티 설정

--- src/main/java/io/github/wonyoungpark/ams/config/SecurityConfig.java
+++ src/main/java/io/github/wonyoungpark/ams/config/SecurityConfig.java
@@ -1,45 +1,93 @@
 package io.github.wonyoungpark.ams.config;
 
+import io.github.wonyoungpark.ams.domain.User;
+import io.github.wonyoungpark.ams.repository.UserRepository;
 import io.github.wonyoungpark.ams.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Created by one0 on 2017. 7. 15..
  */
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity // 웹보안 설정 : WebSecurityConfigurerAdapter를 상속받은 클래스에 선언되어야 함.
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Autowired
-    public void configureGolbal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-        //auth.inMemoryAuthentication().withUser("test").password("1111").roles("ADMIN");
+    private UserService userService;
+
+    /**
+     * userDetailsService와 passwordEncoder를 AuthenticationManagerBuilder에 세팅해 준다.(사용자의 username과 password가 맞는지 확인한다.)
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() { // 사용자 정의 UserDetailService
+
+            /**
+             * Spring-Security에서 User 정보를 조회하기 위한 메서드
+             * @param username
+             * @return
+             * @throws UsernameNotFoundException
+             */
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                User user = userService.findByUsername(username);
+                if(user == null) {
+                    throw new UsernameNotFoundException("회원을 찾을 수 없습니다");
+                } else {
+                    return user;
+                }
+            }
+        });
     }
 
+    /**
+     * 특정 요청에 대해서 시큐리티 설정을 무시하함.
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/font-awesome/**", "/fonts/**", "/img/**", "/js/**", "/webjars/**", "/h2console/**");
+    }
+
+    /**
+     * Http 설정
+     * 설명 : 인가, 로그인, 로그아웃을 설정한다.
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        //http.csrf().ignoringAntMatchers("/h2-console/**");
+
         http.authorizeRequests()
-                .antMatchers("/**").hasRole("ADMIN")
+                .antMatchers("/**").hasRole("USER")
                 .antMatchers("/user/**").hasAnyRole("USER", "ADMIN", "SUPER")
                 .antMatchers("/admin/**").hasAnyRole("ADMIN", "SUPER")
                 .antMatchers("/super/**").hasRole("SUPER")
                 .and()
-                .formLogin().permitAll()
+                .formLogin()
+                .defaultSuccessUrl("/adadadasdadadad", true) // 인증 성공시 이동경로
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll()
                 .and()
-                .logout().permitAll().logoutSuccessUrl("/");
+                .logout().permitAll().logoutSuccessUrl("/logout");
     }
 }
