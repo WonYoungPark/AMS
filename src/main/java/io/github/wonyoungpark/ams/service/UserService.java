03a2e69ae2c9d7c0d8e9dc2c79e66f3b75def963
박원영 <wyparks2@gmail.com>
2017-07-27 23:55:35 +0900

    Update - 유저 서비스 구현체 -> 인터페이스 삭제

--- src/main/java/io/github/wonyoungpark/ams/service/UserService.java
+++ src/main/java/io/github/wonyoungpark/ams/service/UserService.java
@@ -1,14 +1,49 @@
 package io.github.wonyoungpark.ams.service;
 
 import io.github.wonyoungpark.ams.domain.User;
+import io.github.wonyoungpark.ams.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by one0 on 2017. 7. 15..
  */
-public interface UserService {
-    public User findByUsername(String name);
-    public List<User> getUsers();
-    public User getUser(Long id);
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User findByUsername(String name) {
+        return userRepository.findByUsername(name);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public User createUser(User user) {
+
+        String username = user.getUsername();
+        if (userRepository.findByUsername(username) != null) {
+            // TODO - 에러 처리
+            log.error("user duplicated exception. {}", username);
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
 }
