c5a36d6d81181e9a20ea281c30f15134024c4bc2
박원영 <wyparks2@gmail.com>
2017-07-27 23:54:30 +0900

    Update - 소스 정리

--- src/main/java/io/github/wonyoungpark/ams/web/UserController.java
+++ src/main/java/io/github/wonyoungpark/ams/web/UserController.java
@@ -1,7 +1,6 @@
 package io.github.wonyoungpark.ams.web;
 
 import io.github.wonyoungpark.ams.domain.User;
-import io.github.wonyoungpark.ams.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
