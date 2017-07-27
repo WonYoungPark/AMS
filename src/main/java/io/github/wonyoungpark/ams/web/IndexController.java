beeca5f232ca5855843f1b0ae44395c606f465f3
wyparks2 <wyparks2@ecosian.com>
2017-07-27 18:06:25 +0900

    Update - url 추가

--- src/main/java/io/github/wonyoungpark/ams/web/IndexController.java
+++ src/main/java/io/github/wonyoungpark/ams/web/IndexController.java
@@ -1,15 +1,22 @@
 package io.github.wonyoungpark.ams.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by one0 on 2017. 7. 1..
  */
-@RestController
+@Controller
 public class IndexController {
-    @RequestMapping("/")
-    public String get() {
-        return "asdadadad";
+    final String path = "views/auth/";
+
+    @GetMapping("/")
+    public String index() {
+        return path + "index";
+    }
+
+    @GetMapping("/login2")
+    public String login() {
+        return path + "login2";
     }
 }
