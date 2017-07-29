7b07e1738e3ac53d0ce22d3040eb6b0f17f0ac19
wyparks2 <wyparks2@ecosian.com>
2017-07-29 17:03:41 +0900

    Update - Api url 추가

--- src/main/java/io/github/wonyoungpark/ams/config/SecurityConfig.java
+++ src/main/java/io/github/wonyoungpark/ams/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -27,6 +28,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static String LOGIN_API = "/api/login";
+    private static String LOGOUT_API = "/api/logout";
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -87,6 +90,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/**").hasAnyRole("USER", "ADMIN", "SUPER")
                 .antMatchers("/admin/**").hasAnyRole("ADMIN", "SUPER")
                 .antMatchers("/super/**").hasRole("SUPER")
+                .antMatchers(HttpMethod.POST, LOGIN_API).permitAll()
+                .antMatchers(LOGOUT_API).permitAll()
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/adadadasdadadad", true) // 인증 성공시 이동경로
